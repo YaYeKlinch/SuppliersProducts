2ec81c39e4681a411b89a15dad02259e2fa7751c
Ihor <deqque13@gmail.com>
2019-05-14 02:31:47 +0300

    working

--- SuppliersProducts/Program.cs
+++ SuppliersProducts/Program.cs
@@ -6,16 +6,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SuppliersProducts.Data;
 
 namespace SuppliersProducts
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+       
+            public static void Main(string[] args)
+            {
+                var host = CreateWebHostBuilder(args).Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    try
+                    {
+                        var context = services.GetRequiredService<DataBaseContext>();
+                        context.Database.EnsureCreated();
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "An error occurred while seeding the database.");
+                    }
+                }
+
+                host.Run();
+            }
+        
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
