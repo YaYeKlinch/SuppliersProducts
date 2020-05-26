deb296af1e8120e9bc790e6c88f7c298a8ab8cef
Ihor Volokhovych <50595040+antomys@users.noreply.github.com>
2020-05-26 17:38:27 +0300

    Added Registration Hurray!

--- SuppliersProducts/Startup.cs
+++ SuppliersProducts/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -36,6 +37,9 @@ namespace SuppliersProducts
             services.AddDbContext<DataBaseContext>(options =>
        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DataBaseContext>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -56,6 +60,7 @@ namespace SuppliersProducts
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseIdentity();
 
             app.UseMvc(routes =>
             {
